9bef9827b1e000d6b89911a83c6a75da064e1c7a
Deekshith <deekshith3538@gmail.com>
2024-07-19 21:54:24 +0530

    Form and Div has been added separately

--- displayname/src/App.js
+++ displayname/src/App.js
@@ -1,26 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Displayname from './Displayname';
+import logo from "./logo.svg";
+import "./App.css";
+import Displayname from "./Displayname";
 import { useEffect, useState } from "react";
 
 function App() {
   const [firstname, setFirstname] = useState("");
   const [lastname, setLastname] = useState("");
   const [fullname, setFullname] = useState("");
+  const [value, setValue] = useState(0);
 
   const isblank = (e) => {
     e.preventDefault();
-    setFullname(`${firstname}  ${lastname}`)    
+    if (firstname && lastname) {
+      setFullname(`${firstname} ${lastname}`);
+    } else {
+      setFullname("");
+    }
+    
+    // setFullname(`${firstname}  ${lastname}`);
   };
+
   return (
     <div className="App">
-       <form onSubmit={isblank}>
+      <form onSubmit={isblank}>
         <h1>Full Name Display</h1>
         <label htmlFor="fname">First Name:</label>
         <input
           type="text"
           required
           id="fname"
+          value={firstname}
           onChange={(e) => setFirstname(e.target.value)}
         />
         <br />
@@ -35,12 +44,9 @@ function App() {
         <br />
         <button type="submit">Submit</button>
         {/* <input type="submit" value="Submit" /> */}
-        
       </form>
-      {
-       fullname?<div>Full Name: {fullname}</div>:<></>
-      }
-     
+      
+      {fullname ? <div>Full Name: {fullname}</div> : <></>}
     </div>
   );
 }
